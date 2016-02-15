bbd4cfac5ba8e759afab514a9d9d24d7feccb9c6
Adam Brookes <adam.brookes@gmail.com>
2016-02-15 14:39:43 +0000

    Loop over multiple mgWebService operations

--- scrape_all.py
+++ scrape_all.py
@@ -4,26 +4,31 @@ import requests
 
 base_urls = open('urls.txt')
 
-for url in base_urls:
-    url = url.strip().replace('http://', '').replace('https://', '')
-    req_url_base = "{0}/mgWebService.asmx/GetCouncillorsByWard".format(url)
+mgWebService_ops = ['GetCouncillorsByWard','GetParishCouncils']
 
-    print(req_url_base)
-    try:
-        req = requests.get("http://{0}".format(req_url_base))
-    except:
-        req = requests.get("https://{0}".format(req_url_base))
-    path = os.path.join(
-        os.path.abspath('.'),
-        'data',
-        url.replace('http://', '')\
-        .replace('www.', '')\
-        .replace('applications.', '')\
-        .replace('democracy.', '')\
-        .split('/')[0]\
-        .replace('moderngov.', '')\
-        .replace('ww5.', '')
-    )
-    out_filder = os.makedirs(path, exist_ok=True)
-    out_file = open(os.path.join(path, 'CouncillorsByWard.xml'), 'wb')
-    out_file.write(req.content)
+for url in base_urls:
+    for op in mgWebService_ops:
+        url = url.strip().replace('http://', '').replace('https://', '')
+        req_url_base = "{0}/mgWebService.asmx/{1}".format(url,op)
+
+        print(req_url_base)
+        try:
+            req = requests.get("http://{0}".format(req_url_base))
+        except:
+            req = requests.get("https://{0}".format(req_url_base))
+        path = os.path.join(
+        
+            os.path.abspath('.'),
+            'data',
+            url.replace('http://', '')\
+            .replace('www.', '')\
+            .replace('applications.', '')\
+            .replace('democracy.', '')\
+            .split('/')[0]\
+            .replace('moderngov.', '')\
+            .replace('ww5.', '')
+        )
+        out_filder = os.makedirs(path, exist_ok=True)
+        out_file_name = "{0}.xml".format(op.replace('Get',''))
+        out_file = open(os.path.join(path,out_file_name), 'wb')
+        out_file.write(req.content)
